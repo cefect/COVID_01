d867c905dd08dc0f47f6ea303ea3f5fde76b42e8
holysheets <47127171+holysheets@users.noreply.github.com>
2020-03-21 18:08:03 -0600

    Update setup_fix01.py

--- setup_fix01.py
+++ setup_fix01.py
@@ -83,6 +83,7 @@ class COVID19Parameters():
 
         # time from symptom onset to recovery per compartiment
         self.gamma = np.random.uniform(1/6, 1/2.6, s.nbetas) * n_Icomp  # range of serial from 8.2 to 6.5
+        if 'Red' in s.setup_name: self.gamma = np.random.uniform(1/4.1, 1/2.6, s.nbetas) * n_Icomp
         
         if 'low' in s.setup_name: self.R0s = np.random.uniform(.6, 1.3, s.nbetas)   # np.random.uniform(1.5, 2, nbetas)
         if 'mid' in s.setup_name: self.R0s = np.random.uniform(2, 3, s.nbetas)
@@ -123,6 +124,7 @@ def parameters_quick_draw(s, npi):
         sigma = 1/5.2
         n_Icomp = 3
         gamma = np.random.uniform(1/6, 1/2.6) * n_Icomp  # range of serial from 8.2 to 6.5
+        if 'Red' in s.setup_name: gamma = np.random.uniform(1/4.1, 1/2.6) * n_Icomp
         
         if 'low' in s.setup_name: R0s = np.random.uniform(1.5, 2)   # np.random.uniform(1.5, 2, nbetas)
         if 'mid' in s.setup_name: R0s = np.random.uniform(2, 3)
